fba1820924bafbeb25f75b8ea7c9bec8ff5d99d7
aparece1241 <apareceh7@gmail.com>
2022-03-17 17:35:49 +0800

    [UPDATE] - Update header and balance ui

--- resources/views/common/user/header.blade.php
+++ resources/views/common/user/header.blade.php
@@ -2,8 +2,7 @@
 <style>
     .header-container{
         background: #0149AB;
-        padding: 10px;
-        padding-left: 15px;
+        padding: 10 0px;
         box-shadow: 2px 2px 2px dimgrey;
     }
     .burgerMenu {
@@ -15,32 +14,56 @@
     }
     .name{
         font-size: 13px;
-        margin-left: -20px;
+        margin-left: -53px;
         color: white;
         text-align: center;
-        margin-top: 10px;
     }
     .profile{
         background: #fff;
         border-radius: 100%;
         padding: 20px;
-        margin-left: -15px;
-        margin-right: 15px;
+        width: 2em;
+        height: 2em;
+        /* margin-left: -15px;
+        margin-right: 15px; */
     }
+
+    #profile-and-name {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        align-items: center;
+    }
+    #burger-cont {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #profile-cont {
+        display: flex;
+        align-self: center;
+        justify-content: flex-end;
+    }
+
 </style>
 <header>
-    <div class="container header-container">
-        <div class="row">
-            <div class="col-8">
-                <div class="burgerMenu"></div>
-                <div class="burgerMenu"></div>
-                <div class="burgerMenu"></div>
-            </div>
-            <div class="col-2">
-                <p class="name">John Doe</p>
-            </div>
-            <div class="col-2">
-                <div class="profile">
+    <div class="header-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-9" id="burger-cont">
+                    <div>
+                        <div class="burgerMenu"></div>
+                        <div class="burgerMenu"></div>
+                        <div class="burgerMenu"></div>
+                    </div>
+                </div>
+                <div class="col-3" id="profile-cont">
+                    <div id="profile-and-name">
+                        <div style="height: 53%;">
+                            <p class="name">John Doe</p>
+                        </div>
+                        <div class="profile"></div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/user/balance.blade.php
+++ resources/views/user/balance.blade.php
@@ -5,109 +5,129 @@
 @endpush
 @section('content')
     <div class="content">
-        <div class="row">
-            <div class="col text-center balance">
-                Balance
-            </div>
-            <div class="col text-center">
-                Credit
+        <div class="container">
+            <div class="row">
+                <div class="col text-center balance">
+                    Balance
+                </div>
+                <div class="col text-center">
+                    Credit
+                </div>
             </div>
         </div>
 
         <p class="text-balance">Available Balance</p>
          <h2> <span>PhP</span> 100.50</h2>
     </div>
-<div class="container">
     <div class="transaction">
-        <div class="row">
-            <div class="col">
-                <p>Recent Transactions</p>
-            </div>
-            <div class="col right">
-                <h4>+</h4>
+        <div class="container">
+            <div class="trans-cont">
+                <div>
+                    <p>Recent Transactions</p>
+                </div>
+                <div class="col right">
+                    <div id="plus">
+                        <h4>+</h4>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 <div class="history">
-    <div class="row first-row">
-        <div class="col left">
+    <div class="first-row trans-cont">
+        <div class="format-text">
             <h5 class="add">₱ +500.00</h5>
         </div>
-        <div class="col right">
-           <p> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <small><a href="#" class="details">View Details</a></small></p>
         </div>
     </div>
-      <div class="row second-row">
-        <div class="col left">
-           <h5 class="minus"> ₱ -200.00</h5>
+    <div class="second-row trans-cont">
+        <div class="format-text">
+            <h5 class="minus">₱ -200.00</h5>
         </div>
-        <div class="col right">
-            <p> April 01 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-    <div class="row first-row">
-        <div class="col left">
+    <div class="first-row trans-cont">
+        <div class="format-text">
             <h5 class="add">₱ +500.00</h5>
         </div>
-        <div class="col right">
-           <p> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-      <div class="row second-row">
-        <div class="col left">
-           <h5 class="minus"> ₱ -200.00</h5>
+    <div class="second-row trans-cont">
+        <div class="format-text">
+            <h5 class="minus">₱ -200.00</h5>
         </div>
-        <div class="col right">
-            <p> April 01 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-    <div class="row first-row">
-        <div class="col left">
+    <div class="first-row trans-cont">
+        <div class="format-text">
             <h5 class="add">₱ +500.00</h5>
         </div>
-        <div class="col right">
-           <p> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-      <div class="row second-row">
-        <div class="col left">
-           <h5 class="minus"> ₱ -200.00</h5>
+    <div class="second-row trans-cont">
+        <div class="format-text">
+            <h5 class="minus">₱ -200.00</h5>
         </div>
-        <div class="col right">
-            <p> April 01 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-    <div class="row first-row">
-        <div class="col left">
+    <div class="first-row trans-cont">
+        <div class="format-text">
             <h5 class="add">₱ +500.00</h5>
         </div>
-        <div class="col right">
-           <p> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-      <div class="row second-row">
-        <div class="col left">
-           <h5 class="minus"> ₱ -200.00</h5>
+    <div class="second-row trans-cont">
+        <div class="format-text">
+            <h5 class="minus">₱ -200.00</h5>
         </div>
-        <div class="col right">
-            <p> April 01 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-    <div class="row first-row">
-        <div class="col left">
+    <div class="first-row trans-cont">
+        <div class="format-text">
             <h5 class="add">₱ +500.00</h5>
         </div>
-        <div class="col right">
-           <p> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
-      <div class="row second-row">
-        <div class="col left">
-           <h5 class="minus"> ₱ -200.00</h5>
+    <div class="second-row trans-cont">
+        <div class="format-text">
+            <h5 class="minus">₱ -200.00</h5>
         </div>
-        <div class="col right">
-            <p> April 01 <br> <span><a href="#" class="details">View Details</a></span></p>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
+        </div>
+    </div>
+    <div class="first-row trans-cont">
+        <div class="format-text">
+            <h5 class="add">₱ +500.00</h5>
+        </div>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
+        </div>
+    </div>
+    <div class="second-row trans-cont">
+        <div class="format-text">
+            <h5 class="minus">₱ -200.00</h5>
+        </div>
+        <div class="format-text">
+           <p class="right"> April 06 <br> <span><a href="#" class="details">View Details</a></span></p>
         </div>
     </div>
 </div>
