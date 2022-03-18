dce1f7afb534b329856cf6aeeba90f8801ed348a
aparece1241 <apareceh7@gmail.com>
2022-03-18 13:54:22 +0800

    [UPDATE] - add animation for sidebar (not finished)

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Header toggle effect
+ */
+window.onload = () => {
+    let burgerInnerCont = document.getElementById('burger-inner-cont')
+    burgerInnerCont.addEventListener('click', () => {
+        console.log("Clicked!")
+    })
+}
--- resources/views/common/user/header.blade.php
+++ resources/views/common/user/header.blade.php
@@ -3,12 +3,17 @@
 <link rel="stylesheet" href="{{ asset('css/common/header.css') }}">
 <link rel="stylesheet" href="{{ asset('css/common/sidebar.css') }}">
 @endpush
+@section('js')
+    <script src="{{ asset('js/main.js') }}"></script>
+@endsection
+
+{{-- header --}}
 <header>
     <div class="header-container">
         <div class="container">
             <div class="row">
                 <div class="col-9" id="burger-cont">
-                    <div>
+                    <div id="burger-inner-cont">
                         <div class="burgerMenu"></div>
                         <div class="burgerMenu"></div>
                         <div class="burgerMenu"></div>
@@ -27,10 +32,10 @@
     </div>
 </header>
 
-{{-- side bar --}}
-
+{{-- sidebar --}}
 <div class="sidebar-wrapper">
-    <div class="container" id="sub-header-info">
+    <div id="sub-header-info">
+        {{-- sub header info --}}
         <div class="sub-header-divider">
             <div id="sub-header-logo">
                 <img id="logo-image" src="{{ asset('img/logo.jpg') }}" alt="">
@@ -38,6 +43,26 @@
             <div id="sub-header-name">
                 <span id="sub-header-text">CGP Trading & Marketing</span>
             </div>
+        </div>
+        {{-- sub header profile info --}}
+        <div id="sub-header-profile-info">
+            <div id="sub-header-profile-img-cont">
+                <img id="sub-header-profile-img" src="{{ asset('img/logo.jpg') }}" alt="">
+            </div>
+            <div id="sub-header-profile-text-cont">
+                <p id="sub-header-profile-text">JOHN DOE</p>
+            </div>
+            <span id="sub-header-profile-text-sm">01000023422</span>
+        </div>
+        {{-- sidebar-navigation --}}
+        <div id="sidebar-nav-cont">
+            <ul id="sidebar-nav-items-wrapper">
+                <li class="sidebar-nav-item">Account Balance</li>
+                <li class="sidebar-nav-item">Account Credit</li>
+                <li class="sidebar-nav-item">Pay Credit</li>
+                <li class="sidebar-nav-item">About</li>
+                <li class="sidebar-nav-item">Logout</li>
+            </ul>
         </div>
     </div>
 </div>
