2664c16f2e5aa26d47531c7f771886f6178dfe19
info@chronostep.com <info@chronostep.com>
2022-04-20 15:38:38 +0800

    [UPDATE]

--- resources/views/user/credit.blade.php
+++ resources/views/user/credit.blade.php
@@ -56,7 +56,7 @@
             @else
             <div class="first-row trans-cont">
                 <div class="format-text">
-                    @if ($history->transaction_type == "Deposit")
+                    @if ($history->transaction_type == config('const.transactions.add_debts'))
                         <h5 class="add">₱ +{{ number_format($history->available_balance) }}</h5>
                     @else
                         <h5 class="minus">₱ -{{ number_format($history->available_balance) }}</h5>
