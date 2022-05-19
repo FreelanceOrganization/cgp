737a24f48c437bfeb239dc46a1d9d608f2605de2
info@chronostep.com <info@chronostep.com>
2022-05-19 09:05:36 +0800

    [UPDATE] showing error in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,12 +7,19 @@
     <div class="image">
         <img src="{{ asset('img/logo.jpg') }}" alt="cgp_logo" class="logo-image">
     </div>
+    @include('common.admin.pop-ups.returnMessage')
 
     <div class="container-sm">
         <form action="{{ route('login.send') }}" method="post">
             @csrf
             <input type="text" class="inputField" id="username" placeholder="Username" name="email">
+            @error('email')
+                <span style="color:red; font-size:12px">*{{ $message }}</span>
+            @enderror
             <input type="password" class="inputField" id="password" placeholder="Password" name="password">
+            @error('password')
+                <span style="color:red; font-size:12px">*{{ $message }}</span>
+            @enderror
             <button type="submit" class="btn">LOG IN</button>
             <div class="info">
                 <a href="#" class="frgtPswd">Forgot Password?</a>
